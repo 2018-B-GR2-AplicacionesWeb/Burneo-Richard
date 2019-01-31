c5a6f7489ec4a4b90a159361093b70a70273d37b
Richard <rchrdbrneo@gmail.com>
2019-01-30 19:01:11 -0500

    bootstrap
    Bootstrap

--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -1,4 +1,3 @@
-
 // const NestFactory = require('@nestjs/core').NestFactory; //js
 import {NestFactory} from '@nestjs/core'; // ts
 // import * as httpserver from 'http-server'; // js
@@ -6,15 +5,37 @@ import {Options} from 'http-server'; // js
 import {AppModule} from './app.module';
 //import {} from './mi-codigo';
 //const a = require('./mi-codigo').a;
+// const session = require('express-session')
+import * as session from 'express-session';
+import * as session from 'express';
+
+const FileStore = require('session-file-store')(session);
+
 
 async function bootstrap() {
     //console.log(a)
     const app = await NestFactory.create(AppModule);
-
     app.set('view engine', 'ejs');
+    //const FileStore = require('session-file-store')(session);
+    app.use(
+        session({
+            name:'server-session-id',
+            secret: 'No sera de tomar un traguito',
+            resave: false,
+            saveUninitialized: true,
+            cookie: {secure: false},
+            store: new FileStore()
+        })
+    );
+    //configurar el servidor web
+    app.use(express.static('public'))
+
+    // /bootstrap/css/bottstrap.css
+    // /bootstrap/js/bootstrap.js
 
     await app.listen(3000);
 }
+
 
 bootstrap();
 
